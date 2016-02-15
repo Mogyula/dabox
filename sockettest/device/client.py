ce80059a917044cd0437524eb9d94a078f420a14
mogyula <mogyula@gmail.com>
2016-02-15 00:23:51 +0000

    python client has all threads running

--- sockettest/device/client.py
+++ sockettest/device/client.py
@@ -3,7 +3,29 @@ import sys
 import threading
 import socket         
 
+class triggerThread (threading.Thread):
+	def __init__(self, port):
+		threading.Thread.__init__(self)
+	def run(self):
+		pass
+		#Here we'll basically 
+
 class listenerThread (threading.Thread):
+	def __init__(self, port):
+		threading.Thread.__init__(self)
+	def run(self):
+		fromBoxPort = port;
+		toBoxPort = port + 1; 
+
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.bind(('', fromBoxPort))
+		s.listen(3)
+
+		while 1:
+			conn, addr = s.accept()
+			processThread(conn, addr).start()
+
+class processThread (threading.Thread):
     def __init__(self, conn, addr):
         threading.Thread.__init__(self)
         self.conn = conn
@@ -12,9 +34,10 @@ class listenerThread (threading.Thread):
 		print "Connected to %s:%s" % (self.addr[0], self.addr[1])
 		data = self.conn.recv(16)
 		doProcessing(data)
-		conn.close()
+		self.conn.close()
 
 def doProcessing(data):
+	#btw this is the place where we'll reference the listener functions
 	print "Received: %s" % data
 
 #First of all we gonna make sure, that we got only one argument, and that it's a number.
@@ -26,14 +49,7 @@ if not (len(sys.argv)==2 and sys.argv[1].isdigit()):
 #Let's accept everything
 
 port = int(sys.argv[1]);
-fromBoxPort = port;
-toBoxPort = port + 1; 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', fromBoxPort))
-s.listen(3)
+listenerThread(port).start()
+triggerThread(port).start()
 
-while 1:
-	conn, addr = s.accept()
-	thr=listenerThread(conn, addr)
-	thr.start()
